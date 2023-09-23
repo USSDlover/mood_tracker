429ad6e333f52f9e1807e3ebb9d62acc5635fd79
Alireza <a.razinejad@gmail.com>
2023-09-23 15:37:46 +0330

    feat: implement the descriptive mood picker

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { MoodOptionType } from '../types';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
     { emoji: '👨‍💻', description: 'studious' },
     { emoji: '🤔', description: 'pensive' },
     { emoji: '😊', description: 'happy' },
@@ -10,9 +11,19 @@ const moodOptions = [
 ];
 
 export const MoodPicker: React.FC = () => {
+    const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
+
     return <View style={styles.moodOptions}>
         {moodOptions.map(option => (
-            <Text key={option.emoji}>{option.emoji}</Text>
+            <View>
+                <Pressable
+                    onPress={() => setSelectedMood(option)}
+                    style={[styles.modItem, selectedMood?.emoji === option.emoji ? styles.selectedMood : undefined]}
+                >
+                    <Text key={option.emoji}>{option.emoji}</Text>
+                </Pressable>
+                <Text style={styles.descriptionText}>{option.emoji === selectedMood?.emoji ? option.description : undefined}</Text>
+            </View>
         ))}
     </View>
 }
@@ -21,5 +32,23 @@ const styles = StyleSheet.create({
     moodOptions: {
         flexDirection: 'row',
         justifyContent: 'space-between'
+    },
+    modItem: {
+        height: 60,
+        width:60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    selectedMood: {
+        borderColor: '#fff',
+        borderWidth: 2,
+        backgroundColor: '#454C73',
+    },
+    descriptionText: {
+        textAlign: 'center',
+        color: '#454C73',
+        fontWeight: 'bold',
+        fontSize: 10
     }
-})
+});
