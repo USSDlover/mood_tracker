e24b4beeb24a6c6c6c966860c6e77e319ce4dadb
Alireza <a.razinejad@gmail.com>
2023-09-24 11:02:09 +0330

    feat: show the svg icon for bottom navigation tabs

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -2,14 +2,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import { HomeScreen } from './Home.screen';
 import { HistoryScreen } from './History.screen';
+import { Text } from 'react-native';
+import { AnalyticIcon, HistoryIcon, HomeIcon } from '../components/Icons';
+import { AnalyticScreen } from './Analytic.screen';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
     return (
-        <BottomTabs.Navigator>
+        <BottomTabs.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    switch (route.name) {
+                        case 'Home':
+                            return <HomeIcon size={size} color={color} />;
+                        case 'History':
+                            return <HistoryIcon size={size} color={color} />;
+                        case 'Analytics':
+                            return <AnalyticIcon size={size} color={color} />;
+                        default:
+                            return <Text>{route.name}</Text>;
+                    }
+                }
+            })}
+        >
             <BottomTabs.Screen name={'Home'} component={HomeScreen} />
             <BottomTabs.Screen name={'History'} component={HistoryScreen} />
+            <BottomTabs.Screen name={'Analytics'} component={AnalyticScreen} />
         </BottomTabs.Navigator>
     )
 }
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ImageBackground, StyleSheet } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
-import { HomeIcon } from '../components/Icons';
 
 const imageUrl = 'https://media.istockphoto.com/id/1470130937/photo/young-plants-growing-in-a-crack-on-a-concrete-footpath-conquering-adversity-concept.webp?b=1&s=170667a&w=0&k=20&c=IRaA17rmaWOJkmjU_KD29jZo4E6ZtG0niRpIXQN17fc=';
 const background = require('../../assets/background.jpg');
@@ -12,7 +11,6 @@ export const HomeScreen: React.FC = () => {
 
     return (
         <ImageBackground source={background} style={styles.container}>
-            <HomeIcon />
             <MoodPicker handleSelectMood={appContext.handleSelectMood} />
         </ImageBackground>
     )
