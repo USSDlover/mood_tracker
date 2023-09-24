0a359921df4d4d8d97d2b615114b704f0614d936
Alireza <a.razinejad@gmail.com>
2023-09-24 11:21:52 +0330

    feat: replace icon for all the tabs

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -5,6 +5,7 @@ import { HistoryScreen } from './History.screen';
 import { Text } from 'react-native';
 import { AnalyticIcon, HistoryIcon, HomeIcon } from '../components/Icons';
 import { AnalyticScreen } from './Analytic.screen';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -12,6 +13,9 @@ export const BottomTabsNavigator: React.FC = () => {
     return (
         <BottomTabs.Navigator
             screenOptions={({ route }) => ({
+                tabBarActiveTintColor: theme.colorBlue,
+                tabBarInactiveTintColor: theme.colorGrey,
+                tabBarShowLabel: false,
                 tabBarIcon: ({ color, size }) => {
                     switch (route.name) {
                         case 'Home':
@@ -26,9 +30,9 @@ export const BottomTabsNavigator: React.FC = () => {
                 }
             })}
         >
-            <BottomTabs.Screen name={'Home'} component={HomeScreen} />
-            <BottomTabs.Screen name={'History'} component={HistoryScreen} />
-            <BottomTabs.Screen name={'Analytics'} component={AnalyticScreen} />
+            <BottomTabs.Screen name={'Home'} component={HomeScreen} options={{ title: 'Today\'s Mood' }} />
+            <BottomTabs.Screen name={'History'} component={HistoryScreen} options={{ title: 'Past Moods' }} />
+            <BottomTabs.Screen name={'Analytics'} component={AnalyticScreen} options={{ title: 'Fancy Charts' }} />
         </BottomTabs.Navigator>
     )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,7 @@
 export const theme = {
     colorPurple: 'rgba(38,117,182,0.85)',
     colorWhite: '#fff',
-    colorLavender: '#87677B'
+    colorLavender: '#87677B',
+    colorBlue: '#1D84B5',
+    colorGrey: '#8E9AAF'
 }
